8c2a69eae67254006d1c2c5824b499e3d7b729e2
pajecawav <pajecawav@gmail.com>
2022-12-16 13:54:55 +0300

    day 16 part 2

--- 16/main.ts
+++ 16/main.ts
@@ -44,11 +44,20 @@ function parseText(text: string) {
 function printDotGraph(graph: Graph) {
 	console.log("digraph G {");
 	for (const node of Object.values(graph)) {
+		if (node.rate === 0) {
+			console.log(`\t${node.name} [style=filled, fillcolor="red"]`);
+		}
+	}
+	for (const node of Object.values(graph)) {
 		for (const edge of node.edges) {
 			console.log(`\t${node.name} -> ${edge}`);
 		}
 	}
 	console.log("}");
+}
+
+function hashAll(...values: any[]) {
+	return values.join("...");
 }
 
 function part1(text: string) {
@@ -59,6 +68,8 @@ function part1(text: string) {
 	const LIMIT = 30;
 
 	let best = -1;
+
+	const maxPredictions: Record<string, number> = {};
 
 	function dfs(
 		minutes: number,
@@ -84,8 +95,17 @@ function part1(text: string) {
 			return;
 		}
 
+		const hash = hashAll(minutes, node);
+
+		const maxPrediction = total + timeLeft * maxTotalRate;
+		const bestMaxPrediction = maxPredictions[hash] ?? -1;
+		if (bestMaxPrediction >= maxPrediction || maxPrediction <= best) {
+			return;
+		}
+		maxPredictions[hash] = maxPrediction;
+
 		// not enough time to beat best with all cranes open
-		if (total + timeLeft * maxTotalRate <= best) {
+		if (maxPrediction <= best) {
 			return;
 		}
 
@@ -107,7 +127,86 @@ function part1(text: string) {
 	return best;
 }
 
+function part2(text: string) {
+	const { graph, maxTotalRate, goodNodesCount } = parseText(text);
+
+	// return printDotGraph(graph);
+
+	const LIMIT = 26;
+
+	let best = -1;
+
+	const maxPredictions: Record<string, number> = {};
+
+	function dfs(
+		minutes: number,
+		me: string,
+		elephant: string,
+		open: Set<string>,
+		currentRate: number,
+		total: number,
+		prevMe: string | null,
+		prevElephant: string | null
+	) {
+		total += currentRate;
+
+		const timeLeft = LIMIT - minutes;
+
+		// time expired
+		if (timeLeft === 0) {
+			best = Math.max(best, total);
+			return;
+		}
+
+		// all cranes are open
+		if (goodNodesCount === open.size) {
+			best = Math.max(best, total + timeLeft * currentRate);
+			return;
+		}
+
+		const hash = hashAll(minutes, me, elephant);
+
+		const maxPrediction = total + timeLeft * maxTotalRate;
+		const bestMaxPrediction = maxPredictions[hash] ?? -1;
+		if (bestMaxPrediction >= maxPrediction || maxPrediction <= best) {
+			return;
+		}
+		maxPredictions[hash] = maxPrediction;
+
+		// not enough time to beat best with all cranes open
+		if (maxPrediction <= best) {
+			return;
+		}
+
+		function* iterSteps(name: string, prev: string | null) {
+			if (!open.has(name) && graph[name].rate > 0) {
+				open.add(name);
+				currentRate += graph[name].rate;
+				yield name;
+				currentRate -= graph[name].rate;
+				open.delete(name);
+			}
+
+			for (const edge of graph[name].edges) {
+				if (edge !== prev) {
+					yield edge;
+				}
+			}
+		}
+
+		for (const nextMe of iterSteps(me, prevMe)) {
+			for (const nextElephant of iterSteps(elephant, prevElephant)) {
+				dfs(minutes + 1, nextMe, nextElephant, open, currentRate, total, me, elephant);
+			}
+		}
+	}
+
+	dfs(1, "AA", "AA", new Set(), 0, 0, null, null);
+
+	return best;
+}
+
 const text = readInput();
 
 timeit(() => console.log(part1(text), 2029));
-// console.log(part2(text));
+timeit(() => console.log(part2(text), 2723));
